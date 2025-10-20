145248d82419bae39c8ee571a1743104c952a3aa
Oleksandr Karpachov <o.karpachov@mascode.com>
2025-10-20 21:04:07 +0300

    added nav2 + slam

--- verter_admin/src/verter_admin/launch/verter_base.launch.py
+++ verter_admin/src/verter_admin/launch/verter_base.launch.py
@@ -47,6 +47,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -102,7 +103,7 @@ def generate_launch_description():
         output='screen',
         parameters=[{
             'use_sim_time': use_sim_time,
-            'robot_description': Command(['xacro ', urdf_file]),
+            'robot_description': ParameterValue(Command(['xacro ', urdf_file]), value_type=str),
             'publish_frequency': 50.0
         }]
     )
