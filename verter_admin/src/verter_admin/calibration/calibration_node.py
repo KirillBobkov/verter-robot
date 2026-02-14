781f964a3a39c9a78ca0d7c9bc3fdfcf5f961b53
Oleksandr Karpachov <o.karpachov@mascode.com>
2026-02-14 10:08:20 +0200

    calibration

--- verter_admin/src/verter_admin/calibration/calibration_node.py
+++ verter_admin/src/verter_admin/calibration/calibration_node.py
@@ -24,7 +24,7 @@ from nav_msgs.msg import Odometry
 class CalibrationNode(Node):
 
     WHEEL_CIRCUMFERENCE = 0.59
-    WHEEL_BASE = 0.362
+    WHEEL_BASE = 0.361
 
     LINEAR_SPEED = 0.20
     ANGULAR_SPEED = 0.3
