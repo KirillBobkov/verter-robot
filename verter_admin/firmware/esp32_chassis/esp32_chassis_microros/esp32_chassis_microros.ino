b657b38f436b7ea11c952adba3efc2b2fa1bfb96
Oleksandr Karpachov <o.karpachov@mascode.com>
2026-02-14 10:36:56 +0200

    calibration

--- verter_admin/firmware/esp32_chassis/esp32_chassis_microros/esp32_chassis_microros.ino
+++ verter_admin/firmware/esp32_chassis/esp32_chassis_microros/esp32_chassis_microros.ino
@@ -65,7 +65,7 @@
 
 const float WHEEL_CIRCUMFERENCE = 0.59;
 const float GEAR_RATIO = 4.0007;
-const float WHEEL_BASE = 0.369;
+const float WHEEL_BASE = 0.370;
 
 const float MAX_VELOCITY = 0.5;
 const int MAX_PWM = 200;
