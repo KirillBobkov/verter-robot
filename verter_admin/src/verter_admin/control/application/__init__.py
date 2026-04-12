f380b79f600bf9c6fec5a665de716d95f34c197f
Oleksandr Karpachov <o.karpachov@mascode.com>
2026-04-12 12:54:43 +0300

    new lidar node

--- verter_admin/src/verter_admin/control/application/__init__.py
+++ verter_admin/src/verter_admin/control/application/__init__.py
@@ -1,12 +1,11 @@
 """Application layer use-cases for control."""
 
 from .evaluate_safety import EvaluateSafety, SafetyEvaluationInput
-from .compute_odometry import ComputeOdometry, OdometryEvent, OdometrySnapshot
+from .compute_odometry import ComputeOdometry, OdometrySnapshot
 
 __all__ = [
     'EvaluateSafety',
     'SafetyEvaluationInput',
     'ComputeOdometry',
-    'OdometryEvent',
     'OdometrySnapshot',
 ]
